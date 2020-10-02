c057b2c391a277851510500299cec9d670b1556d
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-10-03 02:44:26 +0500

    Update App.jsx
    email/ phone

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ const App = () => {
   const [fullName, setFullName] = useState({
     fname:"",
     lname:"",
+    email:"",
+    phone:"",
   });
   
 
@@ -10,20 +12,44 @@ const inputEvent = (event) => {
   console.log(event.target.value);
   console.log(event.target.name);
 
-  const value = event.target.value;
-  const name = event.target.name;
+  //const value = event.target.value;
+  //const name = event.target.name;
+const { value, name} = event.target;
+
   setFullName((preValue) => {
 console.log(preValue);
 if (name === "fName") {
  return {
    fname: value,
    lname: preValue.lname,
+   email: preValue.email,
+   phone: preValue.phone,
 
   };
-} else if (name === "lName"){
+} else if (name === "lName") {
+  return {
+    fname: value,
+    lname: preValue.lname,
+    email: preValue.email,
+    phone: preValue.phone,
+
+  };
+}
+else if (name === "email") {
   return {
     fname: preValue.fname,
     lname: value,
+    email: preValue.email,
+    phone: preValue.phone,
+
+  };
+}
+else if (name === "phone") {
+  return {
+    fname: preValue.fname,
+    lname: preValue.lname,
+    email:value,
+    phone: preValue.phone,
 
   };
 }
@@ -43,10 +69,18 @@ return(
   <div>
   
   <h1> Hello {fullName.fname} {fullName.lname} </h1> 
+  <p> {fullName.email} </p>
+  <p> {fullName.phone} </p>
   <input type="text" placeholder="Enter Your Name" name="fName"onChange={inputEvent} value={fullName.fname} />
   <br />
+
   <input type="text" placeholder="Enter Your Last Name" name="lName"onChange={inputEvent}
   value={fullName.lname} />
+   <input type="email" placeholder="Enter Your email" name="email"onChange={inputEvent}
+  value={fullName.email} />
+
+   <input type="number" placeholder="Enter Your pjone number" name="phone"onChange={inputEvent}
+  value={fullName.phone} />
   <button type="submit">Submit Me 👍 </button>
 </div>
   </form>
