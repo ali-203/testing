3f8a9eed4f5828b34a1beabf6913efbb636c6818
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-10-09 21:50:13 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,45 +17,53 @@ const inputEvent = (event) => {
 const { value, name} = event.target;
 
   setFullName((preValue) => {
-console.log(preValue);
-if (name === "fName") {
+ console.log(preValue);
  return {
-   fname: value,
-   lname: preValue.lname,
-   email: preValue.email,
-   phone: preValue.phone,
-
+  ...preValue,
+  [name]: value,
+  
   };
-} else if (name === "lName") {
-  return {
-    fname: value,
-    lname: preValue.lname,
-    email: preValue.email,
-    phone: preValue.phone,
 
-  };
-}
-else if (name === "email") {
-  return {
-    fname: preValue.fname,
-    lname: value,
-    email: preValue.email,
-    phone: preValue.phone,
 
-  };
-}
-else if (name === "phone") {
-  return {
-    fname: preValue.fname,
-    lname: preValue.lname,
-    email:value,
-    phone: preValue.phone,
 
-  };
-}
+// if (name === "fName") {
+//  return {
+//    fname: value,
+//    lname: preValue.lname,
+//    email: preValue.email,
+//    phone: preValue.phone,
+
+//   };
+// } else if (name === "lName") {
+//   return {
+//     fname: value,
+//     lname: preValue.lname,
+//     email: preValue.email,
+//     phone: preValue.phone,
+
+//   };
+// }
+// else if (name === "email") {
+//   return {
+//     fname: preValue.fname,
+//     lname: value,
+//     email: preValue.email,
+//     phone: preValue.phone,
+
+//   };
+// }
+// else if (name === "phone") {
+//   return {
+//     fname: preValue.fname,
+//     lname: preValue.lname,
+//     email:value,
+//     phone: preValue.phone,
+
+//   };
+// }
 
   }
-  )
+  );
 };
 const onSubmits = (event) => {
 event.preventDefault();
@@ -71,10 +79,10 @@ return(
   <h1> Hello {fullName.fname} {fullName.lname} </h1> 
   <p> {fullName.email} </p>
   <p> {fullName.phone} </p>
-  <input type="text" placeholder="Enter Your Name" name="fName"onChange={inputEvent} value={fullName.fname} />
+  <input type="text" placeholder="Enter Your Name" name="fname"onChange={inputEvent} value={fullName.fname} />
   <br />
 
-  <input type="text" placeholder="Enter Your Last Name" name="lName"onChange={inputEvent}
+  <input type="text" placeholder="Enter Your Last Name" name="lname"onChange={inputEvent}
   value={fullName.lname} />
    <input type="email" placeholder="Enter Your email" name="email"onChange={inputEvent}
   value={fullName.email}
